1ccd1b1fa064eadce00adcecf3cac9a0c6030a53
Makan <makan.edrisi@gmail.com>
2020-05-10 22:14:45 +0800

    version 1.0.0

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-import { ExampleComponent } from 'use-delayed-state'
-import 'use-delayed-state/dist/index.css'
+import useDelayedState from 'use-delayed-state'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [state, setState, cancelSetState] = useDelayedState('')
+  const [delay, setDelay] = useState(900)
+  const [updating, setUpdating, cancelSetUpdating] = useDelayedState(
+    () => false
+  )
+  function handleChange(e) {
+    setState(e.target.value, delay)
+  }
+
+  useEffect(() => {
+    setUpdating(() => true)
+    setUpdating(() => false, 1000)
+  }, [state])
+
+  return (
+    <div>
+      <p className='description'>
+        In this example while user is typing, state is updated only after
+        passing delay time.
+      </p>
+      <div className='container'>
+        <div className='form-container'>
+          <form>
+            <h4>Real-time value</h4>
+
+            <textarea
+              type='text'
+              placeholder='Type something'
+              onChange={handleChange}
+            />
+            <label htmlFor='delay'>{`Debouncing delay ${delay}ms`}</label>
+            <input
+              type='range'
+              id='delay'
+              name='delay'
+              value={delay}
+              min={0}
+              max={2000}
+              step={20}
+              onChange={(e) => {
+                setDelay(e.target.value)
+              }}
+            />
+          </form>
+        </div>
+        <div className='form-container'>
+          <form>
+            <h4>Debounced State</h4>
+            <p className={updating ? 'updating' : ''}>{state}</p>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-import React from 'react'
-import styles from './styles.module.css'
+import { useEffect, useState, useRef } from 'react'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export default function useDelayedState(initialState) {
+  const [state, setState] = useState(initialState)
+  const timeoutRef = useRef()
+
+  const setStateAfter = (newState, delay) => {
+    if (delay === 0 || delay === undefined) {
+      setState(newState)
+    } else {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+      timeoutRef.current = setTimeout(() => {
+        setState(newState)
+        timeoutRef.current = null
+      }, delay)
+    }
+  }
+
+  const cancelSetState = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current)
+      timeoutRef.current = null
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current)
+    }
+  }, [])
+
+  return [state, setStateAfter, cancelSetState]
 }
